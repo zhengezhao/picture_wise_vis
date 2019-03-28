a18211d03c2349c666777ff2afa4f91ab05cfd0f
zhengezhao <zhengezhao@email.arizona.edu>
2019-03-28 09:56:28 -0700

    add relu to bp

--- utils.py
+++ utils.py
@@ -252,7 +252,9 @@ def GradCamAlgorithm(weight, matrix,up_dim):
 
     upsample = nn.Upsample(size=(up_dim[0],up_dim[1]),mode='bilinear',align_corners=False)
 
-    return upsample(average_tensor).numpy().flatten()
+    upsample_tensor = upsample(average_tensor).numpy().flatten()
+
+    return np.maximum(upsample_tensor,0)
 
 
 
